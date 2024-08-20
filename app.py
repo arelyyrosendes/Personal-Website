ea8cbe08959044dc0f78fa94bb0a4660a3711489
Arely <arelyyyrosendes@gmail.com>
2024-08-19 18:29:43 -0700

    seperate websites

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-print ("Hello")
-# app.py
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -7,6 +5,18 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
+
+@app.route('/hobbies')
+def hobbies():
+    return render_template('hobbies.html')
 
 if __name__ == '__main__':
     app.run(debug=True)

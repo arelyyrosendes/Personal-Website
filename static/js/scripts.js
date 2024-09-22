eeb324008ac00b0363110cb966f44e38a00923af
arely <arely@Arelys-Macbook-Pro.local>
2024-09-22 16:13:23 -0700

    mobile version 1

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,3 +1,4 @@
+// Home File
 const text = "Hi, I'm Arely";
 const typingElement = document.getElementById('typing');
 let index = 0;
@@ -58,3 +59,59 @@ skillContainers.forEach(container => {
         document.body.removeChild(tempInput);
         alert('Email address copied to clipboard!');
     }
+
+    document.addEventListener('DOMContentLoaded', function() {
+        const menuToggle = document.querySelector('.menu-toggle');
+        const headerRight = document.querySelector('.header-right');
+
+        menuToggle.addEventListener('click', function() {
+            menuToggle.classList.toggle('active');
+            headerRight.classList.toggle('active');
+        });
+    });
+
+    document.addEventListener("DOMContentLoaded", function() {
+        const menuToggle = document.querySelector('.menu-toggle');
+        const headerRight = document.querySelector('.header-right');
+    
+        menuToggle.addEventListener('click', function() {
+            headerRight.classList.toggle('active'); // Toggle the 'active' class
+        });
+    });
+    
+    // Projects File
+    const text_project = "My Projects";
+    const typingElement_project = document.getElementById('typing_project');
+    let index_project = 0;
+
+    function type() {
+        if (index_project < text.length
+
+        ) {
+            typingElement_project.textContent += text[index_project];
+            index_project++;
+            setTimeout(type, 100); // Adjust typing speed here (in milliseconds)
+        }
+    }
+
+    window.onload = function() {
+        type();
+    };
+
+    // Resume File
+    const text_resume = "My Resume";
+    const typingElement_resume = document.getElementById('typing_resume');
+    let index_resume = 0;
+
+    function type() {
+        if (index_resume < text.length) {
+            typingElement.textContent += text[index_resume];
+            index_resume++;
+            setTimeout(type, 100); // Adjust typing speed here (in milliseconds)
+        }
+    }
+
+    window.onload = function() {
+        type();
+    };
+
